d9890caffedf333750e4216cfdf5420a717e6120
MaximeSauvage1 <maxime.sauvage1@gmail.com>
2018-05-26 18:54:04 +0200

    assign values

--- test/test_mcts.mjs
+++ test/test_mcts.mjs
@@ -5,7 +5,7 @@ let yellow_win = 0;
 
 let sn = process.argv[2];
 
-for (let i = 0; i < 20; ++i) {
+for (let i = 0; i < 100; ++i) {
   let e = new lib.OpenXum.Invers.Engine(lib.OpenXum.Invers.GameType.STANDARD, lib.OpenXum.Invers.Color.RED);
   let p1 = new lib.OpenXum.RandomPlayer(lib.OpenXum.Invers.Color.RED, e);
   let p2 = new lib.OpenXum.MCTSPlayer(lib.OpenXum.Invers.Color.YELLOW, e, sn);
@@ -17,7 +17,7 @@ for (let i = 0; i < 20; ++i) {
     p = p === p1 ? p2 : p1;
   }
 
-  console.log("Winner is " + (e.winner_is() === lib.OpenXum.Invers.Color.RED ? "red" : "yellow"));
+  //console.log("Winner is " + (e.winner_is() === lib.OpenXum.Invers.Color.RED ? "red" : "yellow"));
   if (e.winner_is() === lib.OpenXum.Invers.Color.RED) {
     red_win++;
   } else {
